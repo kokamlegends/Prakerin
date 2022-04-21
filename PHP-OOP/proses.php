20d415f6cba514c526b57589baf026822e1e93ce
Kaqa Zidane S <90295103+kokamlegends@users.noreply.github.com>
2022-04-21 11:06:37 +0700

    Menambahkan Upload Foto

--- PHP-OOP/proses.php
+++ PHP-OOP/proses.php
@@ -16,7 +16,29 @@ if ($aksi == "tambah") {
 }elseif ($aksi == "update") {
     $db->update($_POST['id'],$_POST['nama'],$_POST['alamat'],$_POST['usia']);
     header("location:tampil.php");
-}
-else{
+}elseif ($aksi == "foto") {
+    $nama = $_POST['nama'];
+    $kontak = $_POST['kontak'];
+    $alamat = $_POST['alamat'];
+
+    $rand = rand();
+    $ekstensi = array('png','jpg','jpeg','gif');
+    $filename = $_FILES['foto']['name'];
+    $ukuran = $_FILES['foto']['size'];
+    $ext = pathinfo($filename, PATHINFO_EXTENSION);
+
+    if (!in_array($ext,$ekstensi)) {
+        header("location:tampil.php?alert=gagal_ekstensi");
+    }else {
+        if ($ukuran < 1044070) {
+            $xx = $rand.'_'.$filename;
+            move_uploaded_file($_FILES['foto']['tmp_name'],'gambar/'.$rand.'_'.$filename);
+            mysqli_query($db->conn, "INSERT INTO user_foto VALUES (NULL,'$nama','$kontak','$alamat','$xx')");
+            header("location:tampil.php?alert=berhasil");
+        }else {
+            header("location:tampil.php?alert=Gagal_Ukuran");
+        }
+    }
+}else{
     echo "<h1 text-align='center'>Halaman Tidak ditemukan !<h1><br><hr>";
 }
--- PHP-OOP/tampil.php
+++ PHP-OOP/tampil.php
@@ -2,11 +2,20 @@
 include "database.php";
 $db = new database();
 ?>
-<h1>CRUD OOP</h1>
-<h2>WWW.MALASNGODING.COM</h2>
-<h3>Data User</h3>
-
-<a href="input.php">Input Data</a>
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+    
+    <h1>CRUD OOP</h1>
+    <h2>WWW.MALASNGODING.COM</h2>
+    <h3>Data User</h3>
+    
+    <a href="input.php">Input Data</a> <br> <hr>
+<a href="up_foto.php">Upload Data + FOTO</a><hr>
 <table border="1px">
     <tr>
         <th>No</th>
@@ -18,7 +27,7 @@ $db = new database();
     <?php
         $no = 1;
         foreach ($db->tampil_data() as $x ) {
-    ?>
+            ?>
     <tr>
         <td><?= $no++; ?></td>
         <td><?= $x['nama'];?></td>
@@ -31,5 +40,30 @@ $db = new database();
     </tr>
     <?php 
         }
+        ?>
+</table>
+
+<!-- Tabel Baru -->
+<table class="table table-bordered">
+    <tr>
+        <th width="20%">Nama</th>
+        <th width="20%">Kontak</th>
+        <th width="40%">Alamat</th>
+        <th width="20%">Foto</th>
+    </tr>
+    <?php 
+    $data = mysqli_query($db->conn,"SELECT * FROM user_foto");
+    while ($d = mysqli_fetch_array($data)) {
+    ?>
+    <tr>
+        <td>  <?= $d['user_nama'] ?>  </td>
+        <td>  <?= $d['user_kontak'] ?>  </td>
+        <td>  <?= $d['user_alamat'] ?>  </td>
+        <td><img src="gambar/<?= $d['user_foto'];?>" width="35" height="40"></td>
+    </tr>
+    <?php    
+    }
     ?>
 </table>
+    
+</body> 
